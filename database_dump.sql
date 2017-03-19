0affb77c3aa17669f56c8be6e762fefffd3a883c
Dan Sirbu <dsirb066@gmail.com>
2017-03-18 22:05:43 -0400

    Updated database dump

--- database_dump.sql
+++ database_dump.sql
@@ -91,6 +91,27 @@ CREATE TABLE company (
 ALTER TABLE company OWNER TO coopcat_dev;
 
 --
+-- Name: company_company_id_seq; Type: SEQUENCE; Schema: public; Owner: coopcat_dev
+--
+
+CREATE SEQUENCE company_company_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE company_company_id_seq OWNER TO coopcat_dev;
+
+--
+-- Name: company_company_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: coopcat_dev
+--
+
+ALTER SEQUENCE company_company_id_seq OWNED BY company.company_id;
+
+
+--
 -- Name: job; Type: TABLE; Schema: public; Owner: coopcat_dev
 --
 
@@ -109,38 +130,66 @@ CREATE TABLE job (
 ALTER TABLE job OWNER TO coopcat_dev;
 
 --
+-- Name: job_job_id_seq; Type: SEQUENCE; Schema: public; Owner: coopcat_dev
+--
+
+CREATE SEQUENCE job_job_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE job_job_id_seq OWNER TO coopcat_dev;
+
+--
+-- Name: job_job_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: coopcat_dev
+--
+
+ALTER SEQUENCE job_job_id_seq OWNED BY job.job_id;
+
+
+--
 -- Name: resume; Type: TABLE; Schema: public; Owner: coopcat_dev
 --
 
 CREATE TABLE resume (
-    "- Resume_ID" numeric(19,0) NOT NULL,
-    "Author- ID" numeric(19,0) NOT NULL,
+    resume_id numeric(19,0) NOT NULL,
+    author_id numeric(19,0) NOT NULL,
     last_updated_at timestamp without time zone NOT NULL,
-    "File Path" character varying(200)
+    resume_path character varying(200)
 );
 
 
 ALTER TABLE resume OWNER TO coopcat_dev;
 
 --
--- Name: review; Type: TABLE; Schema: public; Owner: coopcat_dev
+-- Name: resume_Resume_ID_seq; Type: SEQUENCE; Schema: public; Owner: coopcat_dev
 --
 
-CREATE TABLE review (
-    comments character varying(300) NOT NULL,
-    rating integer NOT NULL,
-    works_forjobjob_id numeric(19,0) NOT NULL,
-    "Works_ForUSER- ID" numeric(19,0) NOT NULL
-);
+CREATE SEQUENCE "resume_Resume_ID_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 
-ALTER TABLE review OWNER TO coopcat_dev;
+ALTER TABLE "resume_Resume_ID_seq" OWNER TO coopcat_dev;
 
 --
--- Name: reviews; Type: TABLE; Schema: public; Owner: coopcat_dev
+-- Name: resume_Resume_ID_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: coopcat_dev
 --
 
-CREATE TABLE reviews (
+ALTER SEQUENCE "resume_Resume_ID_seq" OWNED BY resume.resume_id;
+
+
+--
+-- Name: resume_review; Type: TABLE; Schema: public; Owner: coopcat_dev
+--
+
+CREATE TABLE resume_review (
     resume_id numeric(19,0) NOT NULL,
     moderator_id numeric(19,0) NOT NULL,
     review_date timestamp without time zone NOT NULL,
@@ -148,7 +197,7 @@ CREATE TABLE reviews (
 );
 
 
-ALTER TABLE reviews OWNER TO coopcat_dev;
+ALTER TABLE resume_review OWNER TO coopcat_dev;
 
 --
 -- Name: student; Type: TABLE; Schema: public; Owner: coopcat_dev
@@ -184,7 +233,7 @@ ALTER TABLE user_id_seq OWNER TO coopcat_dev;
 --
 
 CREATE TABLE "user" (
-    id numeric(19,0) DEFAULT nextval('user_id_seq'::regclass) NOT NULL,
+    user_id numeric(19,0) DEFAULT nextval('user_id_seq'::regclass) NOT NULL,
     name character varying(100) NOT NULL,
     password character varying(250) NOT NULL,
     email character varying(100) NOT NULL,
@@ -197,12 +246,26 @@ CREATE TABLE "user" (
 ALTER TABLE "user" OWNER TO coopcat_dev;
 
 --
+-- Name: work_review; Type: TABLE; Schema: public; Owner: coopcat_dev
+--
+
+CREATE TABLE work_review (
+    comments character varying(300) NOT NULL,
+    rating integer NOT NULL,
+    works_forjobjob_id numeric(19,0) NOT NULL,
+    works_for_user_id numeric(19,0) NOT NULL
+);
+
+
+ALTER TABLE work_review OWNER TO coopcat_dev;
+
+--
 -- Name: works_for; Type: TABLE; Schema: public; Owner: coopcat_dev
 --
 
 CREATE TABLE works_for (
-    jobjob_id numeric(19,0) NOT NULL,
-    "USER- ID" numeric(19,0) NOT NULL,
+    job_id numeric(19,0) NOT NULL,
+    user_id numeric(19,0) NOT NULL,
     start_date date NOT NULL,
     end_date date NOT NULL
 );
@@ -211,9 +274,37 @@ CREATE TABLE works_for (
 ALTER TABLE works_for OWNER TO coopcat_dev;
 
 --
+-- Name: company company_id; Type: DEFAULT; Schema: public; Owner: coopcat_dev
+--
+
+ALTER TABLE ONLY company ALTER COLUMN company_id SET DEFAULT nextval('company_company_id_seq'::regclass);
+
+
+--
+-- Name: job job_id; Type: DEFAULT; Schema: public; Owner: coopcat_dev
+--
+
+ALTER TABLE ONLY job ALTER COLUMN job_id SET DEFAULT nextval('job_job_id_seq'::regclass);
+
+
+--
+-- Name: resume resume_id; Type: DEFAULT; Schema: public; Owner: coopcat_dev
+--
+
+ALTER TABLE ONLY resume ALTER COLUMN resume_id SET DEFAULT nextval('"resume_Resume_ID_seq"'::regclass);
+
+
+--
 -- Data for Name: company; Type: TABLE DATA; Schema: public; Owner: coopcat_dev
 --
 
+
+
+--
+-- Name: company_company_id_seq; Type: SEQUENCE SET; Schema: public; Owner: coopcat_dev
+--
+
+SELECT pg_catalog.setval('company_company_id_seq', 1, false);
 
 
 --
@@ -223,19 +314,27 @@ ALTER TABLE works_for OWNER TO coopcat_dev;
 
 
 --
+-- Name: job_job_id_seq; Type: SEQUENCE SET; Schema: public; Owner: coopcat_dev
+--
+
+SELECT pg_catalog.setval('job_job_id_seq', 1, false);
+
+
+--
 -- Data for Name: resume; Type: TABLE DATA; Schema: public; Owner: coopcat_dev
 --
 
 
 
 --
--- Data for Name: review; Type: TABLE DATA; Schema: public; Owner: coopcat_dev
+-- Name: resume_Resume_ID_seq; Type: SEQUENCE SET; Schema: public; Owner: coopcat_dev
 --
 
+SELECT pg_catalog.setval('"resume_Resume_ID_seq"', 1, false);
 
 
 --
--- Data for Name: reviews; Type: TABLE DATA; Schema: public; Owner: coopcat_dev
+-- Data for Name: resume_review; Type: TABLE DATA; Schema: public; Owner: coopcat_dev
 --
 
 
@@ -250,8 +349,8 @@ ALTER TABLE works_for OWNER TO coopcat_dev;
 -- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: coopcat_dev
 --
 
-INSERT INTO "user" (id, name, password, email, address, role) VALUES (4, 'John Doe1', 'password', 'johndoe1@gmail.com', '123 North Street', 1);
-INSERT INTO "user" (id, name, password, email, address, role) VALUES (6, 'John Doe2', 'password', 'johndoe2@gmail.com', '123 North Street', 1);
+INSERT INTO "user" (user_id, name, password, email, address, role) VALUES (4, 'John Doe1', 'password', 'johndoe1@gmail.com', '123 North Street', 1);
+INSERT INTO "user" (user_id, name, password, email, address, role) VALUES (6, 'John Doe2', 'password', 'johndoe2@gmail.com', '123 North Street', 1);
 
 
 --
@@ -262,17 +361,15 @@ SELECT pg_catalog.setval('user_id_seq', 6, true);
 
 
 --
+-- Data for Name: work_review; Type: TABLE DATA; Schema: public; Owner: coopcat_dev
+--
+
+
+
+--
 -- Data for Name: works_for; Type: TABLE DATA; Schema: public; Owner: coopcat_dev
 --
 
-
-
---
--- Name: user USER_email_key; Type: CONSTRAINT; Schema: public; Owner: coopcat_dev
---
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT "USER_email_key" UNIQUE (email);
 
 
 --
@@ -280,7 +377,7 @@ ALTER TABLE ONLY "user"
 --
 
 ALTER TABLE ONLY "user"
-    ADD CONSTRAINT "USER_pkey" PRIMARY KEY (id);
+    ADD CONSTRAINT "USER_pkey" PRIMARY KEY (user_id);
 
 
 --
@@ -300,19 +397,35 @@ ALTER TABLE ONLY job
 
 
 --
--- Name: resume resume_pkey; Type: CONSTRAINT; Schema: public; Owner: coopcat_dev
+-- Name: resume resume_author_id_resume_id_pk; Type: CONSTRAINT; Schema: public; Owner: coopcat_dev
 --
 
 ALTER TABLE ONLY resume
-    ADD CONSTRAINT resume_pkey PRIMARY KEY ("- Resume_ID");
+    ADD CONSTRAINT resume_author_id_resume_id_pk PRIMARY KEY (author_id, resume_id);
 
 
 --
--- Name: reviews reviews_pkey; Type: CONSTRAINT; Schema: public; Owner: coopcat_dev
+-- Name: work_review review_works_forjobjob_id_works_for_user_id_pk; Type: CONSTRAINT; Schema: public; Owner: coopcat_dev
 --
 
-ALTER TABLE ONLY reviews
-    ADD CONSTRAINT reviews_pkey PRIMARY KEY (resume_id);
+ALTER TABLE ONLY work_review
+    ADD CONSTRAINT review_works_forjobjob_id_works_for_user_id_pk PRIMARY KEY (works_forjobjob_id, works_for_user_id);
+
+
+--
+-- Name: resume_review reviews_pkey; Type: CONSTRAINT; Schema: public; Owner: coopcat_dev
+--
+
+ALTER TABLE ONLY resume_review
+    ADD CONSTRAINT reviews_pkey PRIMARY KEY (resume_id, moderator_id);
+
+
+--
+-- Name: student student_user_id_pk; Type: CONSTRAINT; Schema: public; Owner: coopcat_dev
+--
+
+ALTER TABLE ONLY student
+    ADD CONSTRAINT student_user_id_pk PRIMARY KEY (user_id);
 
 
 --
@@ -320,7 +433,14 @@ ALTER TABLE ONLY reviews
 --
 
 ALTER TABLE ONLY works_for
-    ADD CONSTRAINT works_for_pkey PRIMARY KEY (jobjob_id, "USER- ID");
+    ADD CONSTRAINT works_for_pkey PRIMARY KEY (job_id, user_id);
+
+
+--
+-- Name: user_email_uindex; Type: INDEX; Schema: public; Owner: coopcat_dev
+--
+
+CREATE UNIQUE INDEX user_email_uindex ON "user" USING btree (email);
 
 
 --
@@ -331,35 +451,11 @@ CREATE TRIGGER update_num_available_positions_trigger AFTER INSERT ON works_for 
 
 
 --
--- Name: job fkjob188415; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
+-- Name: resume_review fkreviews794469; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
 --
 
-ALTER TABLE ONLY job
-    ADD CONSTRAINT fkjob188415 FOREIGN KEY (company_id) REFERENCES company(company_id);
-
-
---
--- Name: resume fkresume602495; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
---
-
-ALTER TABLE ONLY resume
-    ADD CONSTRAINT fkresume602495 FOREIGN KEY ("Author- ID") REFERENCES "user"(id);
-
-
---
--- Name: reviews fkreviews395856; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
---
-
-ALTER TABLE ONLY reviews
-    ADD CONSTRAINT fkreviews395856 FOREIGN KEY (resume_id) REFERENCES resume("- Resume_ID");
-
-
---
--- Name: reviews fkreviews794469; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
---
-
-ALTER TABLE ONLY reviews
-    ADD CONSTRAINT fkreviews794469 FOREIGN KEY (moderator_id) REFERENCES "user"(id);
+ALTER TABLE ONLY resume_review
+    ADD CONSTRAINT fkreviews794469 FOREIGN KEY (moderator_id) REFERENCES "user"(user_id);
 
 
 --
@@ -367,31 +463,39 @@ ALTER TABLE ONLY reviews
 --
 
 ALTER TABLE ONLY student
-    ADD CONSTRAINT fkstudent664699 FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE;
+    ADD CONSTRAINT fkstudent664699 FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE;
 
 
 --
--- Name: works_for fkworks_for151853; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
+-- Name: job job_company_company_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
+--
+
+ALTER TABLE ONLY job
+    ADD CONSTRAINT job_company_company_id_fk FOREIGN KEY (company_id) REFERENCES company(company_id);
+
+
+--
+-- Name: resume resume_user_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
+--
+
+ALTER TABLE ONLY resume
+    ADD CONSTRAINT resume_user_id_fk FOREIGN KEY (author_id) REFERENCES "user"(user_id);
+
+
+--
+-- Name: works_for works_for_job_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
 --
 
 ALTER TABLE ONLY works_for
-    ADD CONSTRAINT fkworks_for151853 FOREIGN KEY ("USER- ID") REFERENCES "user"(id);
+    ADD CONSTRAINT works_for_job_id_fk FOREIGN KEY (job_id) REFERENCES job(job_id);
 
 
 --
--- Name: works_for fkworks_for677765; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
+-- Name: works_for works_for_user_user_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
 --
 
 ALTER TABLE ONLY works_for
-    ADD CONSTRAINT fkworks_for677765 FOREIGN KEY (jobjob_id) REFERENCES job(job_id);
-
-
---
--- Name: review review; Type: FK CONSTRAINT; Schema: public; Owner: coopcat_dev
---
-
-ALTER TABLE ONLY review
-    ADD CONSTRAINT review FOREIGN KEY (works_forjobjob_id, "Works_ForUSER- ID") REFERENCES works_for(jobjob_id, "USER- ID");
+    ADD CONSTRAINT works_for_user_user_id_fk FOREIGN KEY (user_id) REFERENCES "user"(user_id);
 
 
 --
