80ebbd081ebfe81c33621574c129367f7b13ea48
DanSirbu <dsirb066@gmail.com>
2017-03-27 14:49:15 -0400

    Added resumes to moderator page

--- controller/moderator.go
+++ controller/moderator.go
@@ -3,16 +3,28 @@ package controller
 import (
 	"net/http"
 	"ps_web_0/view"
+	"ps_web_0/db"
 )
 
-const moderatorId  = 10
+//Update to logged in user
+const moderatorId  = 6
 
 func moderatorResumeSummary(w http.ResponseWriter, r *http.Request)  {
 	var data = make(map[string]interface{})
 
 
 	//db.GetModeratorResumes()
-	data["notification"] = "Hmm it works"
+	data["notification"] = "Resumes"
 
+	resumes := db.GetModeratorResumes(moderatorId)
+	var splitResumes = make(map[float64][]*db.UserResume)
+
+	for _, element := range resumes {
+		splitResumes[element.UserID] = append(splitResumes[element.UserID], element)
+	}
+
+	data["user_resumes"] = splitResumes
+
+	//db.GetModeratorResumes(moderatorId)[0].ResumeReview.Review
 	view.RenderTemplate(w, "moderator_summary", data)
 }
